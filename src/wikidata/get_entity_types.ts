d94068f2650d847f403aeb02f48f7a8dc7b3f824
Dumitru Cantea <canteadumitru@gmail.com>
2018-07-02 15:01:51 +0300

    getEntityTypesByName

--- src/wikidata/get_entity_types.ts
+++ src/wikidata/get_entity_types.ts
@@ -15,13 +15,13 @@ const PREFIXES_MAP: StringPlainObject = {
 const PREFIXES = Object.keys(PREFIXES_MAP).map(key => PREFIXES_MAP[key]);
 const PREFIXES_REG = new RegExp('^(' + Object.keys(PREFIXES_MAP).join('|') + ')');
 
-export function getEntityTypes(id: string, prefixes?: string[]): Promise<string[]> {
+export function getEntityTypesByName(name: string, prefixes?: string[]): Promise<string[]> {
     if (!prefixes || !prefixes.length) {
         prefixes = PREFIXES;
     }
 
-    return dbpediaWikidataTypes(id)
-        .then((types) => parseTypes(types))
+    return dbpediaTypes(name)
+        .then(types => parseTypes(types))
         .then(types => {
             return types.filter(item => PREFIXES_REG.test(item))
                 .map(item => {
@@ -35,21 +35,21 @@ function parseTypes(types: any[]): string[] {
     return types.map(item => item.type.value);
 }
 
-function dbpediaWikidataTypes(id: string): Promise<any[]> {
-    const query = `PREFIX dbpedia-wikidata: <http://wikidata.dbpedia.org/resource/>
-SELECT ?type
-WHERE {
-dbpedia-wikidata:${id} rdf:type ?type
-}`;
-    return request<any>({ url: 'http://wikidata.dbpedia.org/sparql', qs: { query: query }, timeout: 10 * 1000 })
-        .then(data => data.results && data.results.bindings);
-}
+// function dbpediaWikidataTypes(id: string): Promise<any[]> {
+//     const query = `PREFIX dbpedia-wikidata: <http://wikidata.dbpedia.org/resource/>
+// SELECT ?type
+// WHERE {
+// dbpedia-wikidata:${id} rdf:type ?type
+// }`;
+//     return request<any>({ url: 'http://dbpedia.org/sparql', qs: { query: query }, timeout: 10 * 1000 })
+//         .then(data => data.results && data.results.bindings);
+// }
 
 function dbpediaTypes(name: string): Promise<any[]> {
-    const query = `PREFIX dbr: <http://dbpedia.org/resource/>
+    const query = `
 SELECT ?type
 WHERE {
-dbr:${name.replace(/\s+/, ' ')} rdf:type ?type
+    <http://dbpedia.org/resource/${name.replace(/\s+/g, '_')}> rdf:type ?type
 }`;
     return request<any>({ url: 'http://dbpedia.org/sparql', qs: { query: query }, timeout: 10 * 1000 })
         .then(data => data.results && data.results.bindings);
